9832f221fe385ab887eb327746f6483b075ece30
MayderC <maydercortes1@gmail.com>
2022-03-28 14:36:16 -0600

    project base

---
+++ WebApplication3/Controllers/UsersController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebApplication3.Jwt;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Authorization;
+
+namespace WebApplication3.Controllers
+{
+  [ApiController]
+  [Route("[controller]")]
+  public class UsersController : ControllerBase
+  {
+
+    private readonly ILogger<UsersController> _logger;
+    private readonly IConfiguration _config;
+    public UsersController(ILogger<UsersController> logger, IConfiguration config)
+    {
+      _logger = logger;
+      this._config = config;
+    }
+
+
+    [HttpPost("login")]
+    public ActionResult<object> createJWT([FromBody] User user)
+    {
+      var keySecret = this._config.GetValue<string>("Secret");
+      var jwt = new JsonWebToken(keySecret);
+      var myToken = jwt.createJWT(user);
+      return Ok(new
+      {
+        ok = true,
+        token = myToken
+      });
+    }
+
+    [HttpGet]
+    [Authorize]
+    public ActionResult<List<User>> GetUsers()
+    {
+      var data = ContainerData.GetInstance();
+      return Ok(data.GetUsers());
+    }
+
+    [HttpGet("id")]
+    public ActionResult<List<User>> GetUser(int id)
+    {
+      var data = ContainerData.GetInstance();
+      return Ok(data.GetUser(id));
+    }
+
+    [HttpPost]
+    public ActionResult<List<User>> AddUsers([FromBody] User user)
+    {
+      var data = ContainerData.GetInstance();
+      data.AddUser(user);
+      return Ok(data.GetUsers());
+    }
+
+    [HttpDelete("id")]
+    public ActionResult RemoveUser(int id)
+    {
+      var data = ContainerData.GetInstance();
+      bool resp = data.RemoveUser(id);
+      return Ok(resp);
+    }
+  }
+
+
+  public class User
+  {
+
+    public int id { get; set; }
+    public string name { get; set; }
+    public string password { get; set; }
+    public string rol { get; set; }
+  }
+}
---
+++ WebApplication3/Helpers/JsonWebToken.cs
@@ -0,0 +1,42 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.Text;
+using WebApplication3.Controllers;
+using System;
+
+namespace WebApplication3.Jwt
+{
+  public class JsonWebToken
+  {
+    private readonly byte[] _secret;
+
+    public JsonWebToken(string @secretkey)
+    {
+      _secret = Encoding.ASCII.GetBytes(secretkey);
+    }
+
+
+    public string createJWT(User user)
+    {
+      var claims = new ClaimsIdentity();
+
+      claims.AddClaim(new Claim(ClaimTypes.NameIdentifier, user.name));
+      claims.AddClaim(new Claim(ClaimTypes.NameIdentifier, user.id + ""));
+      claims.AddClaim(new Claim(ClaimTypes.Role, user.rol));
+
+      var tokenDesc = new SecurityTokenDescriptor()
+      {
+        Subject = claims,
+        Expires = DateTime.UtcNow.AddHours(5),
+        SigningCredentials =
+              new SigningCredentials(new SymmetricSecurityKey(_secret),
+                  SecurityAlgorithms.HmacSha256Signature)
+      };
+
+      var tokenHandler = new JwtSecurityTokenHandler();
+      var token = tokenHandler.CreateToken(tokenDesc);
+      return tokenHandler.WriteToken(token);
+    }
+  }
+}
